f423634e583bae0e8c48aeb9f25dcdb204e74c43
luishenrick <l.henrick@live.com>
2021-11-23 01:13:58 -0300

    operadores_aritimeticos_js

---
+++ UC 09 - Programação Client-side/operadores_aritimeticos.js
@@ -0,0 +1,14 @@
+// operadores aritimeticos
+var valorUm = 10;
+var valorDois = 4;
+
+var total;
+
+total = valorUm + valorDois;        //adição
+total = valorUm - valorDois;            //subtração
+total = valorUm * valorDois;                //multiplicação
+total = valorUm / valorDois;         //divisão
+total = 10 / 3;                         //potenciação
+//total = 3 ** 2;                                //retso da divisão
+console.log(total);
+
