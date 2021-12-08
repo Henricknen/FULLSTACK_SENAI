a385ff813939e3b3791f5140e770ca43f70e2e3c
luishenrick <l.henrick@live.com>
2021-12-08 08:58:49 -0300

    switch

---
+++ UC 09 - Programação Client--side/comando_selecao.js
@@ -0,0 +1,15 @@
+var tipoUsuario = "qualquer cargo";
+
+switch(tipoUsuario) {
+    case "adim":        // caso
+        console.log("Bem vindo adim...");
+        break;
+    case "Gerente":
+        console.log("Bem vindo Gerente...");
+        break;
+    case "Secretario:":
+        console.log("Bem vindo secretario...");
+        break;
+    default:
+        console.log("Bem vindo anonimo");
+}
