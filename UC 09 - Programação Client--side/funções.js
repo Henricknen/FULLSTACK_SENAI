eaac7ee69e56c66d72103ea5ac730081eb2795e0
luishenrick <l.henrick@live.com>
2021-12-08 22:34:18 -0300

    com_e_sem_parametro/argumento

---
+++ UC 09 - Programação Client--side/funções.js
@@ -0,0 +1,11 @@
+function exibirMenssagem() {            // função_sem_parametro_e_sem_argumento
+    console.log("Luis_henrique_s_f_");
+}
+exibirMenssagem();
+
+
+function media(notaUm, notaDois) {      // função_com_parametro
+    resultado = (notaUm + notaDois) / 2;
+    return resultado;
+}
+console.log(media(10, 6));
