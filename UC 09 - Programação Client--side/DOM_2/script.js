93a38007b0b772fb4f12f190253d2cce7ee6c9dc
luishenrick <l.henrick@live.com>
2021-12-10 16:24:13 -0300

    getElementById_setTimeout

---
+++ UC 09 - Programação Client--side/DOM_2/script.js
@@ -0,0 +1,7 @@
+//document.getElementById('idCustomizado').innerHTML = "<b>Conteudo HTML do JS</b>";
+
+document.getElementById('idCustomizadoDois').innerText = "Luis_henrique_silva_ferreira"; // considera_apenas_texto
+
+setTimeout(function() {     // da_um_tempo_para executar_algo
+    document.getElementById('idCustomizado').innerHTML = "<b>Conteudo HTML do JS</b>";
+}, 3000);       // executa_apos_3_segundos
