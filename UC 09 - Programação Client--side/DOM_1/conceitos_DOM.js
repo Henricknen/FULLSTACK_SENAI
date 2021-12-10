f3ea374ed33081cb1a9dad41da743311f625e325
luishenrick <l.henrick@live.com>
2021-12-10 15:42:18 -0300

    conceitos_de_DOM__document_title_body_write

---
+++ UC 09 - Programação Client--side/DOM_1/conceitos_DOM.js
@@ -0,0 +1,6 @@
+document.title = "JavaScript - DOM alterado";              // "document" faz referencia a pagina "conceitos_DOM.html"
+
+document.body.style.background = "blue";
+//document.body.style.backgroundImage = "url('img/codigo.jpg')";
+
+document.write("<h2>Author</h2> <br> <h1>Luis_henrique_silva_ferreira</h1>");
