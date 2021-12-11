60cb50e5efd43ceb033b004ed56fe6be67913938
luishenrick <l.henrick@live.com>
2021-12-11 17:59:10 -0300

    getElementsByTagName_setTimeout

---
+++ UC 09 - Programação Client--side/DOM_5/tag_name.js
@@ -0,0 +1,10 @@
+setTimeout (function (){
+    document.getElementsByTagName('h1')[2].innerHTML = 'selecionado_tag_p';
+}, 3000);
+
+setTimeout (function (){
+    document.getElementsByTagName('div')[2].innerHTML = 'selecionado_tag_p';
+}, 3500);
+setTimeout (function (){
+    document.getElementsByTagName('p')[2].innerHTML = 'selecionado_tag_p';
+}, 4000);
