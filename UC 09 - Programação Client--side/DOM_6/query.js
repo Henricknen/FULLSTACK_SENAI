1a9d2d34a1960dc394b48c85bf751915a5dddda3
luishenrick <l.henrick@live.com>
2021-12-11 18:57:15 -0300

    querySelector_e_querySelectorAll

---
+++ UC 09 - Programação Client--side/DOM_6/query.js
@@ -0,0 +1,15 @@
+// querySelector
+document.querySelector('#idqualquer4').innerHTML = "<b>conteudo_da_id_idqualquer4</b>";
+
+document.querySelector('#idqualquer8').innerHTML = "<b>conteudo_da_id_idqualquer8</b>";
+
+document.querySelector('p').innerHTML = "conteudo_da_tag_p";
+
+document.querySelector('.classqualquer1').innerText = "texto_comum_sem_HTML";
+
+// querySelectorAll
+document.querySelectorAll('#idqualquer')[2].innerHTML = "QuerySelectorAll_id";
+
+document.querySelectorAll('.classqualquer')[0].innerText = "QuerySelectorAll_class";
+
+document.querySelectorAll('div')[0].innerHTML = "div_7";
