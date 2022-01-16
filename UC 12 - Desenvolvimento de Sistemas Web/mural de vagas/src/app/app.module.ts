7b53fbd13d9e012f785fb5c670723e2d9f334a71
Henricknen <l.henrick@live.com>
2022-01-16 00:02:55 -0300

    instalando_o_bootstrap

---
+++ UC 12 - Desenvolvimento de Sistemas Web/mural de vagas/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';               // app.Moule.ts - é_um_conjunto_de_blibliotecas_que_tem_varios_componentes_dentro
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
